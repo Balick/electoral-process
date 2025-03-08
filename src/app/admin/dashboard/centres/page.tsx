e2a0cf3f7bcbfa957053e8435d8e90df8a2a2c29
Théo Balick <balickmethens@gmail.com>
2025-03-08 21:35:59 +0100

    Ajout du tableau de données pour les centres avec TanStack React Table
    - Intégration de @tanstack/react-table comme nouvelle dépendance
    - Création d'un composant DataTable pour afficher les centres avec leurs gestionnaires
    - Mise à jour de la page des centres pour récupérer et afficher les données dynamiquement
    - Implémentation des colonnes et de la structure de données pour le tableau des centres

--- src/app/admin/dashboard/centres/page.tsx
+++ src/app/admin/dashboard/centres/page.tsx
@@ -1,6 +1,23 @@
+import { getCenters, getManagers } from "@/lib/supabase/center";
 import LogOut from "../_components/log-out";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
 
-export default function Page() {
+export default async function Page() {
+  const centers = await getCenters();
+  const managers = await getManagers();
+
+  const centersWithManagers = centers.map((center) => {
+    const managerForCenter = managers?.find((manager) => {
+      return manager.center_id === center.id;
+    });
+
+    return {
+      ...center,
+      manager: managerForCenter || null,
+    };
+  });
+
   return (
     <div>
       <div className="flex gap-4 justify-between items-center flex-wrap">
@@ -10,6 +27,10 @@ export default function Page() {
         <div className="flex gap-2 items-center flex-wrap flex-row-reverse">
           <LogOut />
         </div>
+      </div>
+
+      <div className="my-8">
+        <DataTable columns={columns} data={centersWithManagers} />
       </div>
     </div>
   );
---
+++ src/lib/supabase/center.ts
@@ -0,0 +1,88 @@
+"use server";
+
+import { Center } from "@/app/admin/dashboard/centres/_components/columns";
+import { createClient } from "@/lib/supabase/server";
+
+// getCenters function to get all centers from the centers table
+export async function getCenters(): Promise<Center[]> {
+  const supabase = createClient();
+  const { data, error } = await supabase.from("centres").select("*");
+
+  if (error) {
+    console.error(
+      `❌   Erreur lors de la récupération des centres : `,
+      error.message
+    );
+  }
+
+  return data as Center[];
+}
+
+// getCenter function to get a center from the centers table
+export async function getCenter(id: string): Promise<Center | null> {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("centres")
+    .select("*")
+    .eq("id", id);
+
+  if (error) {
+    console.error(
+      `❌   Erreur lors de la récupération du centre ${id} : `,
+      error.message
+    );
+  }
+
+  return data?.[0] as Center;
+}
+
+// getCenterByName function to get a center from the centers table
+export async function getCenterByName(name: string): Promise<Center | null> {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("centres")
+    .select("*")
+    .eq("nom", name);
+
+  if (error) {
+    console.error(
+      `❌   Erreur lors de la récupération du centre ${name} : `,
+      error.message
+    );
+  }
+
+  return data?.[0] as Center;
+}
+
+// getManager function to get a manager from the users table
+export async function getManager(id_center: string) {
+  const supabase = createClient();
+
+  const { data, error: errorUser } = await supabase
+    .from("users")
+    .select("*")
+    .eq("center_id", id_center);
+
+  if (errorUser) {
+    console.error(
+      `❌   Erreur lors de la récupération du manager du centre ${id_center} : `,
+      errorUser.message
+    );
+  }
+
+  return data;
+}
+
+export async function getManagers() {
+  const supabase = createClient();
+  const { data, error } = await supabase.from("users").select("*");
+
+  if (error) {
+    console.error(
+      `❌   Erreur lors de la récupération des managers : `,
+      error.message
+    );
+  }
+
+  return data;
+}
